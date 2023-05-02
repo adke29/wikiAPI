973762de8a763cbe19dfff319c0f800a2ab5bc33
Kevin Adrian <adrian.kevin@yahoo.com>
2023-05-02 17:33:26 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+//setup express
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+//setup mongoose
+//mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
+mongoose.connect("mongodb+srv://admin-kevin:Test123456@cluster0.6ehnxuj.mongodb.net/wikiDB");
+const articleSchema = new mongoose.Schema({
+    title:{type:String,required:true},
+    content:{type:String,required:true}
+});
+const Articles = mongoose.model("articles",articleSchema);
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to Wiki DB");
+})
+
+
+//Routing
+app.route("/articles")
+.get((req,res)=>{
+    console.log("New Request")
+    Articles.find({}).then((articles)=>{
+        res.send(articles);
+    })
+})
+.post(async (req,res)=>{
+    console.log("New Post!!");
+    console.log(req.body);
+    newArticle = new Articles({
+        title: req.body.title,
+        content: req.body.content
+    });
+    await newArticle.save().then(()=>{
+        res.send("New post Created!!");
+    }).catch(()=>{
+        res.send(400);
+    });   
+})
+.delete((req,res)=>{
+    Articles.deleteMany({}).then(()=>{
+        res.send("Successfully deleted: " + targetId);
+    }).catch(()=>{
+        res.send(400);
+    })
+});
+
+
+
+app.route("/articles/:articlesId")
+.get((req,res)=>{
+    Articles.findOne({_id:req.params.articlesId}).then((article)=>{
+        res.send(article);
+    })
+
+})
+.put((req,res)=>{
+    Articles.findOneAndUpdate(
+        {_id:req.params.articlesId},
+        {
+            title: req.body.title,
+            content: req.body.content
+        },
+        { new: true, overwrite: true }
+        ).then(()=>{
+            res.send(req.params.articlesId + " successfully updated")
+        }).catch((err) =>{
+            console.log(err)
+            res.sendStatus(400);
+        })
+})
+.patch((req,res)=>{
+    Articles.findOneAndUpdate({_id:req.params.articlesId},{$set:req.body}).then(()=>{
+        res.send(req.params.articlesId + " successfully updated")})
+        .catch((err) =>{
+            console.log(err)
+            res.sendStatus(400);
+        })
+})
+.delete((req,res)=>{
+    console.log(req.params.articlesId)
+    Articles.deleteOne({_id:req.params.articlesId}).then(()=>{
+        res.send("Successfully deleted: ");
+    }).catch(()=>{
+        res.sendStatus(400);
+    })
+})
+
+//listen
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Server running");
+});
